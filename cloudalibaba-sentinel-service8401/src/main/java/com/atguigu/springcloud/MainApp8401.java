a9abcb93eb736cc9486b1c3b416dcd43d2809650
renyy <renyyit@163.com>
2020-05-13 22:52:17 +0800

    本次更新： 1.sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author renyuyu
+ * @Description:
+ * @date 2020-05-13 9:54 下午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+     public static void main(String[] args) {
+           SpringApplication.run(MainApp8401.class, args);
+      }
+
+}
